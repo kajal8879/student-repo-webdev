b9ccad0fc1d4b976fbbf6a58945fdab7a5a03732
Kajal Patil <kajal@pdx.edu>
2023-04-17 15:11:48 -0700

    Homework 1 submission

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(
+    "My name is Kajal Patil and I am pursuing masters at Portland State University. I joined in last Fall term for Computer Scinece course. My focus is on software engineering and database track. Apart from studies I am exploring calm and blooming spots nearby."
+  );
 }
